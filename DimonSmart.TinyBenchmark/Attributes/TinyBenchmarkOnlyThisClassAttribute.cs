b38e0f720add6c3e0d35c8364f1fe385f90ea53a
Dmitry Dorogoy <dorogoj@live.ru>
2023-11-28 21:36:00 +0100

    Documentation updated. Attributes comments added

--- DimonSmart.TinyBenchmark/Attributes/TinyBenchmarkOnlyThisClassAttribute.cs
+++ DimonSmart.TinyBenchmark/Attributes/TinyBenchmarkOnlyThisClassAttribute.cs
@@ -1,5 +1,10 @@
 ﻿namespace DimonSmart.TinyBenchmark.Attributes;
 
+/// <summary>
+/// Attribute to be applied to benchmark classes, indicating that only benchmarks in the marked class should be considered
+/// during testing. This attribute is useful for focusing changes on a specific test during development without marking
+/// all other tests as ignored. It helps keep the Git changes list clean and focused on the active test.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public sealed class TinyBenchmarkOnlyThisClassAttribute : Attribute
 {
